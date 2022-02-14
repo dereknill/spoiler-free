83f15196c653f4c624b1dcbde9679337f5d63dc1
Derek Nill <derekclancynill@gmail.com>
2022-02-14 15:54:06 -0500

    a

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,4 @@
 import { Link } from "react-router-dom";
-import { firebaseApp } from "../index";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars, faSearch } from "@fortawesome/free-solid-svg-icons";
 import MainLogo from "./MainLogo";

f86321899000ab856b783dfe9acfb0ed83eac29b
Derek Nill <nillderek@gmail.com>
2022-02-22 22:05:39 -0500

    transition

--- src/components/Page.js
+++ src/components/Page.js
@@ -22,7 +22,7 @@ function Page(props) {
   return (
     <div>
       <div
-        className={`w-full h-full bg-black/75 fixed top-0 left-0 right-0 bottom-0 z-[10500] ${
+        className={`w-full h-full bg-black/75 fixed top-0 left-0 right-0 transition-all bottom-0 z-[10500] ${
           !faded && "hidden"
         }`}
         onClick={() => {

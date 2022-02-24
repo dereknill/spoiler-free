5a6b686976b5a7751948738fd0d317bd2f42fd17
Derek Nill <derekclancynill@gmail.com>
2022-02-24 12:48:10 -0500

    Fiixed select screen

--- src/components/Discussion.js
+++ src/components/Discussion.js
@@ -1,8 +1,8 @@
 import { useOutletContext, useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { db } from "../index";
-import { doc, getDoc } from "firebase/firestore";
-// import { uuid } from "uuidv4";
+import { doc, getDoc, setDoc } from "firebase/firestore";
+import uuid from "react-uuid";
 
 function Discussion(props) {
   const [details, user] = useOutletContext();
@@ -10,33 +10,99 @@ function Discussion(props) {
   const [shows, setShows] = useState(null);
   const navigate = useNavigate();
 
-  function displaySeasons(theDetails, theUser) {
-    console.log(shows);
+  function handleWatchChange(event) {
+    event.preventDefault();
+    const season = event.target.getAttribute("season");
+    const episode = event.target.getAttribute("episode");
+    let showList = { ...shows };
+    showList[details.id] = {
+      season: season,
+      episode: episode,
+    };
+    setShows(showList);
+    if (user) {
+      const docRef = doc(db, "users", user.uid);
+      const setResult = async () =>
+        await setDoc(docRef, {
+          shows: showList,
+        });
+
+      setResult();
+    }
+  }
+  function displaySeasons(theDetails, userShows) {
     const seasons = theDetails.seasons;
+    const seasonWatched = userShows[theDetails.id]
+      ? userShows[details.id].season
+      : -1;
+
+    const episodeWatched = userShows[theDetails.id]
+      ? userShows[details.id].episode
+      : -1;
+
     return seasons.map((season) => {
+      let watched;
+      if (season.season_number === 0) {
+        return null;
+      }
+      if (parseInt(season.season_number) < seasonWatched) {
+        watched = "all";
+      } else if (parseInt(season.season_number) === parseInt(seasonWatched)) {
+        watched = "some";
+      } else {
+        watched = "none";
+      }
       return (
-        <div className='grid grid-cols-[10rem, 1fr]' key={season.season_number}>
-          <h3 className='font-bold text-lg'>Season {season.season_number}</h3>
-          <div className='grid grid-cols-10'>
-            {getEpisodeArray(season.episode_count)}
+        <div
+          className='grid grid-cols-1 md:grid-cols-[10rem_1fr] my-4'
+          key={season.season_number}
+        >
+          <h3 className='font-bold text-xl'>Season {season.season_number}</h3>
+          <div className='grid grid-cols-5 md:grid-cols-10 gap-1'>
+            {getEpisodeArray(
+              season.episode_count,
+              watched,
+              episodeWatched,
+              season.season_number
+            )}
           </div>
         </div>
       );
     });
   }
 
-  function getEpisodeArray(count) {
+  function getEpisodeArray(count, seasonWatched, episodeWatched, seasonNumber) {
     let episodes = [];
+
     for (let i = 1; i <= count; i++) {
-      episodes.push(<div key={i}>{i}</div>);
+      let bg;
+      if (seasonWatched === "all") {
+        bg = "bg-green-800";
+      } else if (seasonWatched === "none") {
+        bg = "bg-red-800";
+      } else {
+        if (i <= episodeWatched) {
+          bg = "bg-green-800";
+        } else {
+          bg = "bg-red-800";
+        }
+      }
+      episodes.push(
+        <button
+          key={uuid()}
+          className={`${bg} bg-red-800 border-black border text-center text-white p-1`}
+          episode={i}
+          season={seasonNumber}
+          onClick={handleWatchChange}
+        >
+          {i}
+        </button>
+      );
     }
     return episodes;
   }
 
   useEffect(() => {
-    if (!user && ready) {
-      navigate("/signin");
-    }
     if (user) {
       const docRef = doc(db, "users", user.uid);
       const getResult = async () => await getDoc(docRef);
@@ -46,21 +112,22 @@ function Discussion(props) {
         setReady(true);
       });
     }
-  }, [user]);
+  }, [user, navigate]);
 
-  if (!ready) {
+  if (!ready || !shows) {
     return <div>Loading...</div>;
+  }
+  if (!user) {
+    navigate("/signin");
   }
 
   return (
     <div className='my-4 flex justify-center flex-col'>
       <section className='mx-7'>
         <h2 className='text-3xl font-bold'>{details.name}</h2>
-        <h3 className=''>
-          Showing comments through episode {displaySeasons(details)}
-        </h3>
+        <h3 className=''>Select the episode you have watched through</h3>
       </section>
-      <section className='mx-7'></section>
+      <section className='mx-7'>{displaySeasons(details, shows)}</section>
     </div>
   );
 }
